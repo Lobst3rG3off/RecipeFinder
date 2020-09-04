ce53f5c38ebdc64dbc4b7be7383bd3179705731e
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-09-04 16:02:11 -0400

    now beings the searchbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+
+
+  const APP_ID = '27d2b96c';
+
+  const APP_KEY = 'fc78a95dae4a41c9c6d6d14f5787f383';
+
+
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+
+
   return(
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit"></button>
+      </form>
     </div>
   )
 }
