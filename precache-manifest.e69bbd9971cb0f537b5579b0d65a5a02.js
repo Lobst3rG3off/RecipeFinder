a9fd8947babeb1c572662d57792ed2c3f31d0ce2
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-09-04 19:04:29 -0400

    Updates

---
+++ precache-manifest.e69bbd9971cb0f537b5579b0d65a5a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621aeb9fab3e27ae0eeb3464070358d5",
+    "url": "/RecipieFinder/index.html"
+  },
+  {
+    "revision": "af1324631831d99ce5b5",
+    "url": "/RecipieFinder/static/css/main.ff9c2083.chunk.css"
+  },
+  {
+    "revision": "0dbb17bc94c917c29449",
+    "url": "/RecipieFinder/static/js/2.16cdcfba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipieFinder/static/js/2.16cdcfba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1324631831d99ce5b5",
+    "url": "/RecipieFinder/static/js/main.59faed63.chunk.js"
+  },
+  {
+    "revision": "de65bcd577e8b782c60c",
+    "url": "/RecipieFinder/static/js/runtime-main.d0e70cf4.js"
+  }
+]);
