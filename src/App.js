f27d6abe40dfaf6591d533d13a473cc856dced76
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-09-04 17:47:59 -0400

    page only updates and calls on submit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,16 @@ const App = () => {
   const APP_KEY = 'fc78a95dae4a41c9c6d6d14f5787f383';
   
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] =useState('')
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('Chicken')
 
 
 useEffect(() => {
 getRecipes();
-}, []);
+}, [query]);
 
 const getRecipes = async () => {
- const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+ const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
  const data = await response.json();
  setRecipes(data.hits)
  console.log(data.hits)
@@ -25,12 +26,17 @@ const getRecipes = async () => {
 
 const updateSearch = e => {
  setSearch(e.target.value)
- console.log(e.target.value)
+ console.log(search)
+}
+
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search)
 }
 
   return(
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">
            Search
