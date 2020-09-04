0daafed6d56e6494fb4867573093ddea5bcbf341
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-09-04 16:37:21 -0400

    using useEffect to call the APi once

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
 
   const APP_ID = '27d2b96c';
-
   const APP_KEY = 'fc78a95dae4a41c9c6d6d14f5787f383';
-
-
   const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
 
+const [counter, setCounter] =  useState(0);
+
+useEffect(() => {
+  console.log('Effect has been run')
+}, []);
 
   return(
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
-        <button className="search-button" type="submit"></button>
+        <button className="search-button" type="submit">
+           Search
+           </button>
       </form>
+
+      <h1 onClick={() => setCounter(counter + 1)}> {counter}</h1>
     </div>
   )
 }
